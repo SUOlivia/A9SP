d76e1103938888eb5b1ecbd854f10a42c66e9a93
Ordim3n <cedrik.ducharme@gmail.com>
2017-10-21 16:07:05 -0400

    Implement LED color codes along with a delay of around a second

--- userland/source/main.c
+++ userland/source/main.c
@@ -5,22 +5,23 @@
 #include <sys/types.h>
 #include <3ds.h>
 
+#include "LED.h"
+	
+#define path_splash "romfs:/splash.bin"
+#define path_payloadrfs "romfs:/payload.firm"
+#define path_chainloader "romfs:/A9SP.firm"
+#define path_chainloader_sha "romfs:/A9SP.firm.sha"
+#define path_default "boot0.firm"
+#define path_bootfirm "boot.firm"
+#define FIRM_MAXSIZE (0x3FFB00)
+
 int main() {
     romfsInit();
 	
 	gfxInitDefault();
     consoleInit(GFX_BOTTOM, NULL);
 	
-	#define path_splash "romfs:/splash.bin"
-	#define path_payloadrfs "romfs:/payload.firm"
-	#define path_chainloader "romfs:/A9SP.firm"
-	#define path_chainloader_sha "romfs:/A9SP.firm.sha"
-	#define path_default "boot0.firm"
-	#define path_bootfirm "boot.firm"
-	#define FIRM_MAXSIZE (0x3FFB00)
-	
 	u8* contents;
-	
 	
     FILE *splash = fopen(path_splash, "rb");
 	fseek(splash, 0, SEEK_END);
@@ -34,17 +35,22 @@ int main() {
 	gfxSwapBuffers();
 	gspWaitForVBlank();
 	
-	u8 *buf = (u8*) linearMemAlign(FIRM_MAXSIZE + 0x200, 0x400000);
+	u8 *buf = (u8*) linearMemAlign(FIRM_MAXSIZE, 0x400000);
+	memcpy(buf, "A9NC", 4);
 	
 	FILE *payload = fopen(path_payloadrfs, "r");
 	fseek(payload, 0, SEEK_END);
 	size_t payload_size = ftell(payload);
 	rewind(payload);
-	if(fread(buf + 0x200, 1, payload_size, payload) != payload_size) printf("Failed copy to RAM");
-	memcpy(buf, "A9NC", 4);
+	if(fread(buf + 0x200, 1, payload_size, payload) != payload_size)
+		fixcolor(255, 0, 0);
+	else
+		fixcolor(0, 255, 0);
+	
 	fclose(payload);
 	
-	for(int i=0; i<=120; i++)	for(int i=0; i<=120; i++);
+	for(int i=0; i<=60; i++) gspWaitForVBlank();
+	stfuled();
 	APT_HardwareResetAsync();
 	
     gfxExit();
